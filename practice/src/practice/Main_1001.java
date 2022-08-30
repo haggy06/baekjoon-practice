d46da60d6d210e2fbf6a21a46eff8725f94c6f97
haggy06 <baegjonghyeog80@gmail.com>
2022-08-30 16:52:24 +0900

    Add Baekjoon - 1001

---
+++ practice/src/practice/Main_1001.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Main_1001 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int A, B;
+		
+		A = scan.nextInt();
+		B = scan.nextInt();
+			
+		System.out.print (A - B);
+	}
+}
