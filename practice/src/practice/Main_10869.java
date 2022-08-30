39b7fdce16a764d988ce0668626525c932ee1a67
haggy06 <baegjonghyeog80@gmail.com>
2022-08-30 17:13:40 +0900

    Add Baekjoon - 10869

---
+++ practice/src/practice/Main_10869.java
@@ -0,0 +1,21 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Main_10869 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int A, B;
+		
+		A = scan.nextInt();
+		B = scan.nextInt();
+		scan.close();
+			
+		System.out.println (A + B);
+		System.out.println (A - B);
+		System.out.println (A * B);
+		System.out.println (A / B);
+		System.out.println (A % B);
+	}
+}
