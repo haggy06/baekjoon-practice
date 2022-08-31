e45183f705c8e0ff3f2356ba7fbbf730a05b1a6e
haggy06 <baegjonghyeog80@gmail.com>
2022-08-31 19:54:23 +0900

    Add Baekjoon - 10171

---
+++ practice/src/practice/Main_10171.java
@@ -0,0 +1,11 @@
+package practice;
+
+public class Main_10171 {
+	
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
