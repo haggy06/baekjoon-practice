e9a22013c6413dc38d7ec2feb3ec637b954341ef
haggy06 <baegjonghyeog80@gmail.com>
2022-08-31 20:00:32 +0900

    Add Baekjoon - 25083

---
+++ practice/src/practice/Main_25083.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Main_25083 {
+
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
