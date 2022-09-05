91d63372646aa11352c91b699382e7ef5a0f9ac3
haggy06 <baegjonghyeog80@gmail.com>
2022-09-05 20:53:49 +0900

    Add Baekjoon - 2438

---
+++ practice/src/level_3/Main_2438.java
@@ -0,0 +1,17 @@
+package level_3;
+
+import java.util.Scanner;
+
+public class Main_2438 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		scan.close();
+		
+		for ( int i = 1; i <= N; i++ ) {
+			for ( int j = 0; j < i; j++ ) System.out.print( "*" );
+			System.out.println( "" );
+		}
+	}
+}
