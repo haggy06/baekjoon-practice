37b9a43f421d261866e5d0f7d2d7a3359f11d20a
haggy06 <baegjonghyeog80@gmail.com>
2022-09-07 20:29:30 +0900

    Add Baekjoon - 10951

---
+++ practice/src/level_3/Main_10951.java
@@ -0,0 +1,17 @@
+package level_3;
+
+import java.util.Scanner;
+
+public class Main_10951 {
+
+	public static void main(String[] args) {
+		int A, B;
+		
+		Scanner scan = new Scanner(System.in);
+		while ( scan.hasNext() ) {
+			A = scan.nextInt();
+			B = scan.nextInt();
+			System.out.println( A + B );
+		}
+	}
+}
