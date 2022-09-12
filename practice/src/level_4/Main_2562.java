ed6c9c2755f7c9a39d042aaccb1d8017339f0960
haggy06 <baegjonghyeog80@gmail.com>
2022-09-12 19:53:32 +0900

    Add Baekjoon - 2562

---
+++ practice/src/level_4/Main_2562.java
@@ -0,0 +1,22 @@
+package level_4;
+
+import java.util.Scanner;
+
+public class Main_2562 {
+
+	public static void main(String[] args) {
+		int[] list = new int[ 9 ];
+		
+		Scanner scan = new Scanner(System.in);
+		list[ 0 ] = scan.nextInt();
+		int L = list[ 0 ], num = 0;
+		for ( int i = 1; i < 9; i++ ) {
+			list[ i ] = scan.nextInt();
+			if ( L < list[ i ] ) {
+				L = list[ i ];
+				num = i + 1;
+			}
+		}
+		System.out.print( L + "\n" + num );
+	}
+}
