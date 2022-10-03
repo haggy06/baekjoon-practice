71eea0907dcfc4ec81fdf44d4ec28142034f02d1
haggy06 <baegjonghyeog80@gmail.com>
2022-10-03 19:45:55 +0900

    Add Beakjoon -11654

---
+++ practice/src/level_6/Main_11654.java
@@ -0,0 +1,14 @@
+package level_6;
+
+import java.util.Scanner;
+
+public class Main_11654 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String x = scan.next();
+		scan.close();
+		
+		System.out.print( Integer.valueOf(x.charAt(0)) );
+	}
+}
